a080f640e0dfe9039c027e539dd0232bd380aaa5
loiminh <loiho11082015@gmail.com>
2020-02-14 08:52:44 +0700

    add file

---
+++ src/UCLN.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class UCLN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a");
+        int a = scanner.nextInt();
+        a = Math.abs(a); //tra ve gia tri tuyet doi//
+
+        System.out.println("Enter b");
+        int b = scanner.nextInt();
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println("greatest common factor : " + a);
+    }
+}
